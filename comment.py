096f305bb3ce620731cca2c8cb419018ac945d05
Ranjithkumar Viswanathan <138715287+RKReddy212@users.noreply.github.com>
2023-10-08 18:49:30 +0530

    Update comment.py

--- comment.py
+++ comment.py
@@ -22,3 +22,38 @@ def cmntdetls(vid_id):
 
 
   return CmntDetls
+
+
+
+#To get all comment details of all video ids in the channel
+
+
+
+
+def cmntdetls(vid_id):
+  CmntDetls = []
+  for i in vid_id:
+
+    request = youtube.commentThreads().list(
+      part="snippet,replies",
+      videoId = i,
+      maxResults=2
+      )
+    response = request.execute()
+    try:   ------------------------->#using try and except to get comments without restrictedcomments
+    
+
+
+      for Comment in response['items']:
+        Comment_details = { 'comment' : Comment['snippet']['topLevelComment']['snippet'].get('textOriginal','NA'),
+                            'author_name' : Comment['snippet']['topLevelComment']['snippet'].get('authorDisplayName','NA'),
+                            'authorchnlid': Comment['snippet']['topLevelComment']['snippet']['authorChannelId'].get('value','NA'),
+                            'cmntrply'  :   Comment['snippet'].get('totalReplyCount',0),
+                            'publishdate':  Comment['snippet']['topLevelComment']['snippet'].get('publishedAt','NA')}
+
+        CmntDetls.append(Comment_details)
+    except:
+      pass    
+
+
+  return CmntDetls
